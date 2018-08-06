813bd407bfb4e1bb1d39c3df672526b2738693ca
Matthew Edwards <msdedwards@hachisoft.com>
2018-08-06 15:59:55 -0700

    create svg-icon component

--- src/ui/components/svg/helper-test.ts
+++
@@ -1,9 +0,0 @@
-import svg from './helper';
-
-const { module, test } = QUnit;
-
-module('Helper: svg', function(hooks) {
-  test('it computes', function(assert) {
-    assert.equal(svg([]), undefined);
-  });
-});
--- src/ui/components/svg/helper.ts
+++
@@ -1,3 +0,0 @@
-export default function svg(params) {
-    return `<object type="image/svg+xml" data="${params}.svg">Your browser does not support SVG</object>`;
-}
